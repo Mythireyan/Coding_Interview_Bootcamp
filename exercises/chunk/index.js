94c388373afc2fec767680edec2d7da7c4fac9c5
Mythireyan.P <mythireyanp@gmail.com>
2022-08-14 01:37:56 +0530

    Fizz Buzz and Array Chunck

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,46 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // #1 Solution
+  // const chunckedArr = [];
+
+  // for (let item of array) {
+  //   const lastElement = chunckedArr?.length - 1;
+
+  //   if (lastElement < 0 || chunckedArr[lastElement]?.length === size) {
+  //     chunckedArr?.push([item]);
+  //   } else {
+  //     chunckedArr[lastElement] = [...chunckedArr[lastElement], item];
+  //   }
+  // }
+  // return chunckedArr;
+
+  // ======================================================================================
+  // #2 Solution
+  // const chunckedArr = [];
+
+  // for (let num of array) {
+  //   lastItem = chunckedArr[chunckedArr?.length - 1];
+
+  //   if (!lastItem || lastItem?.length === size) {
+  //     chunckedArr?.push([num]);
+  //   } else {
+  //     lastItem.push(num);
+  //   }
+  // }
+  // return chunckedArr;
+
+  // ======================================================================================
+  // #3 Solution
+  const chunckedArr = [];
+
+  let startIndex = 0;
+  while (startIndex <= array.length - 1) {
+    chunckedArr.push(array.slice(startIndex, startIndex + size));
+    startIndex = startIndex + size;
+  }
+  return chunckedArr;
+}
 
 module.exports = chunk;
